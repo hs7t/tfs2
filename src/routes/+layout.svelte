143a38a679887256aeae850067c52ecfe35acd04
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-12 17:59:40 -0500

    prettify code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	let favicon = `
+    let favicon = `
 		data:image/svg+xml,
 		<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
 			<text y=%22.9em%22 font-size=%2290%22>🏭</text>
 		</svg>
 	`
 
-	let { children } = $props();
+    let { children } = $props()
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children()}
