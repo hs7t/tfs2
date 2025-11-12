24b4c6ad5aec6b0c2f9ecc4080f503bafa781de5
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-12 17:50:27 -0500

    update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	let favicon = `
+		data:image/svg+xml,
+		<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+			<text y=%22.9em%22 font-size=%2290%22>🏭</text>
+		</svg>
+	`
 
 	let { children } = $props();
 </script>
