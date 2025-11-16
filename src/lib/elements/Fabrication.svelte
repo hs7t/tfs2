28db3b10a550f4b30b365ddfca38587534a76cc7
Alí Sinisterra <haidarconh@gmail.com>
2025-11-16 18:44:20 -0500

    create Fabrication section using LargeButton

---
+++ src/lib/elements/Fabrication.svelte
@@ -0,0 +1,17 @@
+<script>
+    import LargeButton from './components/LargeButton.svelte'
+</script>
+
+<div class="fabrication">
+    <LargeButton action={() => {}}>Fabricate</LargeButton>
+</div>
+
+<style>
+    .fabrication {
+        flex-grow: 1;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
