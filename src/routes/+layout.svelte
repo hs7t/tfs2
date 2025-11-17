dc932f66e67e23fcbcac2cfce856b80d074bf7dd
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-17 09:56:02 -0500

    embed Red Hat Mono from Google Fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,13 +5,23 @@
 			<text y=%22.9em%22 font-size=%2290%22>🏭</text>
 		</svg>
 	`
-	import '$lib/styling/style.css'
+    import '$lib/styling/style.css'
 
     let { children } = $props()
 </script>
 
 <svelte:head>
     <link rel="icon" href={favicon} />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin="anonymous"
+    />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Red+Hat+Mono:ital,wght@0,300..700;1,300..700&display=swap"
+        rel="stylesheet"
+    />
 </svelte:head>
 
 {@render children()}
