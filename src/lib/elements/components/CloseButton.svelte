fdeac461ed4b38a906d9ffe607e452f43e742246
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-17 10:54:20 -0500

    create Dialog and CloseButton

---
+++ src/lib/elements/components/CloseButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    let { action } = $props()
+</script>
+
+<button aria-label="Close" title="Close" onclick={action}>
+    <span role="presentation">X</span>
+</button>
+
+<style>
+    button {
+        all: unset;
+        font-size: inherit;
+        padding: 0.5ch;
+        background-color: var(--t-color-accent-A);
+        color: var(--t-color-primary);
+        border: var(--t-border-primary);
+    }
+</style>
