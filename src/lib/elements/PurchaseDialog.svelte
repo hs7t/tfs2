6831b2c343702a5364fc288328506f890afd34ac
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-18 16:23:42 -0500

    handle item availability in PurchaseDialog

--- src/lib/elements/PurchaseDialog.svelte
+++ src/lib/elements/PurchaseDialog.svelte
@@ -5,26 +5,38 @@
     import Dialog from './components/Dialog.svelte'
     let { shown = $bindable(false) } = $props()
 
+    type ItemWithAvailability = Item & {
+        available: boolean
+    }
+
     const getAvailableItems = (currentGame: Game | undefined) => {
-        let result: Array<Item> = []
+        let result: Array<ItemWithAvailability> = []
 
         if (currentGame == undefined) return result
 
+        const getItemIds = (items: Array<Item>) => {
+            let result: Array<string> = []
+            for (let item of items) {
+                result.push(item.id)
+            }
+            return result
+        }
+
         for (let item of ITEMS) {
-            const getItemIds = (items: Array<Item>) => {
-                let result: Array<string> = []
-                for (let item of items) {
-                    result.push(item.id)
-                }
-                return result
+            let itemAvailable: boolean = false
+
+            if (item.id in getItemIds(currentGame.currentState.items)) {
+                itemAvailable = false
             }
 
-            if (item.id in getItemIds(currentGame.currentState.items)) continue
+            if (item.currencyCost > currentGame.currentState.wealth.currency) {
+                itemAvailable = false
+            }
 
-            if (item.currencyCost > currentGame.currentState.wealth.currency)
-                continue
-
-            result.push(item)
+            result.push({
+                ...item,
+                available: itemAvailable,
+            })
         }
 
         return result
@@ -34,12 +46,17 @@
 </script>
 
 <Dialog title="Purchase" bind:shown>
-    {#each availableItems as item}
-        <li>
-            {item.name} ({item.currencyCost})
-            <button onclick={() => game.current?.purchase(item)}
-                >Purchase</button
-            >
-        </li>
-    {/each}
+    {#if game.current}
+        {#each availableItems as item}
+            <li class={item.available ? 'available' : 'unavailable'}>
+                {item.name} ({item.currencyCost})
+                <button
+                    onclick={() => game.current?.purchase(item)}
+                    disabled={!item.available}
+                >
+                    Purchase
+                </button>
+            </li>
+        {/each}
+    {/if}
 </Dialog>
