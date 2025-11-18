e9e6c73c46bb7ec814561057fdb3f6908078cc4c
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-18 18:20:18 -0500

    STYLE sALEdIALOG

--- src/lib/elements/SaleDialog.svelte
+++ src/lib/elements/SaleDialog.svelte
@@ -6,8 +6,35 @@
 </script>
 
 <Dialog title="Sell" {shown}>
-    <input type="number" bind:value={selectedAmount} />
-    <button onclick={() => game.current?.sellTubip(selectedAmount)}
-        >Sell {selectedAmount} tubip</button
-    >
+    <section>
+        <input type="number" bind:value={selectedAmount} />
+        <button onclick={() => game.current?.sellTubip(selectedAmount)}
+            >Sell {selectedAmount} tubip</button
+        >
+    </section>
 </Dialog>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5ch;
+
+        padding: 1ch;
+        padding-top: 0;
+    }
+
+    input[type='number'],
+    button {
+        padding: 0.5ch;
+        font-size: inherit;
+        font-family: inherit;
+
+        border: var(--t-border-primary);
+        border-radius: inherit;
+    }
+
+    button {
+        background-color: var(--t-color-accent-C);
+    }
+</style>
