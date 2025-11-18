972791cbeb04c565dede1ab6c851e23e26af77a6
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-18 13:46:40 -0500

    find available items in PurchaseDialog

--- src/lib/elements/PurchaseDialog.svelte
+++ src/lib/elements/PurchaseDialog.svelte
@@ -1,8 +1,37 @@
-<script>
+<script lang="ts">
+    import type { Item } from '$lib/logic/purchases'
+    import { ITEMS } from '$lib/logic/purchases'
+    import { game, type Game } from '$lib/logic/shared.svelte'
     import Dialog from './components/Dialog.svelte'
     let { shown = $bindable(false) } = $props()
+
+    let shopItems = [] as Array<Item>
+
+    const getAvailableItems = (currentGame: Game | undefined) => {
+        let result: Array<Item> = []
+
+        if (currentGame == undefined) return result
+
+        for (let item of ITEMS) {
+            const getItemIds = (items: Array<Item>) => {
+                let result: Array<string> = []
+                for (let item of items) {
+                    result.push(item.id)
+                }
+                return result
+            }
+
+            if (item.id in getItemIds(currentGame.currentState.items)) {
+                continue
+            }
+
+            result.push(item)
+        }
+
+        return result
+    }
+
+    const availableItems = $derived(getAvailableItems(game.current))
 </script>
 
-<Dialog title="shop" bind:shown>
-    <div>Hello, world!</div>
-</Dialog>
+<Dialog title="Purchase" bind:shown>Hello, world!</Dialog>
