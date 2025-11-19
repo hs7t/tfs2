c0eba95b097e475fff9ca65b85f1a0cba10b76e7
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-19 14:59:30 -0500

    add more generic headlines

--- src/lib/logic/happenings.svelte.ts
+++ src/lib/logic/happenings.svelte.ts
@@ -4,6 +4,7 @@ import {
     type GameActionTarget,
     type GameActionType,
     type GameEffect,
+    type MatterDerivationChangeGameAction,
     type TubipProductionChangeGameAction,
 } from './game.svelte'
 import type { Tickstamp } from './time'
@@ -53,7 +54,67 @@ export const GENERIC_NEWS_UPDATES: Array<NewsUpdate> = [
                 kind: 'modifier',
             },
         ],
-    } as NewsUpdate,
+    },
+    {
+        headline: 'Fears of bubble grow as tubip economy on the rise',
+        maxRepetitions: 1,
+        effects: [],
+    },
+    {
+        headline:
+            "I'm so tired, cries TFS dev after reaching 35 hours developing initially silly idea",
+        maxRepetitions: 1,
+        effects: [],
+    },
+    {
+        headline:
+            "The Jort Company introduces 'Shorts', jorts made out of non-jean material",
+        maxRepetitions: 1,
+        effects: [],
+    },
+    {
+        headline:
+            'They are a good quantity and well-written enough, says government chief about headlines',
+        maxRepetitions: 2,
+        effects: [],
+    },
+    {
+        headline: 'Matter-holding hedge fund shuts down, liquidates assets',
+        maxRepetitions: 1,
+        effects: [
+            {
+                action: {
+                    type: 'change',
+                    target: 'matterDerivation',
+                    actionOptions: {
+                        amount: 2,
+                    },
+                } as MatterDerivationChangeGameAction,
+                kind: 'modifier',
+            },
+        ],
+    },
+    {
+        headline:
+            "Industry and professionals warn against dangerous tubip 'recycling' trend: 'Possibly catastrophic'",
+        maxRepetitions: 2,
+        effects: [],
+    },
+    {
+        headline: 'Scientists finding more ways to use tubip',
+        maxRepetitions: 1,
+        effects: [],
+    },
+    {
+        headline: 'Benefits found to acknowledging tubip at least once a day',
+        maxRepetitions: 1,
+        effects: [],
+    },
+    {
+        headline: 'Meow!',
+        maxRepetitions: 1,
+        effects: [],
+    },
 ]
 
 const newsUpdatesToConsumable = (
